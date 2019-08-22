e99e6bba26d233f7ea9009925e58f2763885a043
Raven Robertson <ravewillow6383@gmail.com>
2019-08-22 15:07:31 -0700

    added plane.js, plane.css

---
+++ frontend_snakes/src/Plane.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import './Plane.css';
+
+class Plane extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            player: {
+                x_pos: 0,
+                y_pos: 0,
+
+            },
+            rows : [[
+                {
+                    cell_type : 'entrance',
+                    x_pos: 0,
+                    y_pos: 0,
+                },
+                {
+                    cell_type : 'aisle',
+                    x_pos: 1,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'aisle',
+                    x_pos: 2,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'aisle',
+                    x_pos: 3,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'aisle',
+                    x_pos: 4,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'seat',
+                    x_pos: 5,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'seat',
+                    x_pos: 6,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'seat',
+                    x_pos: 7,
+                    y_pos: 0,
+
+                },
+                {
+                    cell_type : 'wall',
+                },
+            ],[
+                {
+                    cell_type : 'wall',
+                },
+                {
+                    cell_type : 'seat',
+
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'aisle',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'wall',
+                },
+            ],[
+                {
+                    cell_type : 'wall',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'aisle',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'wall',
+                },
+            ],[
+                {
+                    cell_type : 'wall',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'aisle',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'seat',
+                },
+                {
+                    cell_type : 'wall',
+                },
+            ],
+        ]
+        }
+    }
+
+    renderRows(){
+
+        const rowsToRender = []
+
+        for (let row of this.state.rows) {
+
+
+            let cellsToRender = row.map(cell => {
+
+                let className = 'Cell ' + cell.cell_type
+
+                if (cell.x_pos === this.state.player.x_pos &&
+                    cell.y_pos === this.state.player.y_pos) {
+
+                        className += ' player' 
+                    }
+
+               return<div className={className}>{cell.cell_type}</div>
+        })
+
+            rowsToRender.push(cellsToRender)
+
+        }
+        return rowsToRender
+    }
+
+    render(){
+
+        return <div className="Plane">
+            {this.renderRows()}
+        </div>
+
+    }
+}
+
+export default Plane
